5c6346c017ea5275410c6e3fa09e731bb8fed3a9
Peter Kneale <PeterKneale@gmail.com>
2012-02-29 23:43:08 +1100

    Formatting

--- FriendlyDateTime/FriendlyDateTime/Models/AuctionViewModel.cs
+++ FriendlyDateTime/FriendlyDateTime/Models/AuctionViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using AutoMapper;
+﻿using AutoMapper;
 using FriendlyDateTime.Code.Domain;
 using FriendlyDateTime.Code.Extensions;
 
@@ -8,22 +7,25 @@ namespace FriendlyDateTime.Models
     public class AuctionViewModel
     {
         public string Name { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime StartedAt { get; set; }
-        public DateTime EndedAt { get; set; }
-
         public string FriendlyCreatedAt { get; set; }
         public string FriendlyStartedAt { get; set; }
         public string FriendlyEndedAt { get; set; }
     }
+
     public class AuctionViewModelMappings
     {
         public static void Setup()
         {
             Mapper.CreateMap<Auction, AuctionViewModel>()
-                .ForMember(dst => dst.FriendlyCreatedAt, opt => opt.MapFrom(src => src.CreatedAt.ToFriendlyString()))
-                .ForMember(dst => dst.FriendlyStartedAt, opt => opt.MapFrom(src => src.StartedAt.ToFriendlyString()))
-                .ForMember(dst => dst.FriendlyEndedAt, opt => opt.MapFrom(src => src.EndedAt.ToFriendlyString()));
+                .ForMember(
+                            dst => dst.FriendlyCreatedAt, 
+                            opt => opt.MapFrom(src => src.CreatedAt.ToFriendlyString()))
+                .ForMember(
+                            dst => dst.FriendlyStartedAt, 
+                            opt => opt.MapFrom(src => src.StartedAt.ToFriendlyString()))
+                .ForMember(
+                            dst => dst.FriendlyEndedAt,
+                            opt => opt.MapFrom(src => src.EndedAt.ToFriendlyString()));
         }
     }
 }
